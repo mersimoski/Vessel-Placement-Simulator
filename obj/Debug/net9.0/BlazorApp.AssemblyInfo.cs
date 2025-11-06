5f569674c13bed8466975671ae43992789ee2ed4
Mahir <im.mahirmersimoski@gmail.com>
2025-11-06 01:02:02 +0100

    Regenerate build artifacts for BlazorApp, including updated assembly information, static web assets, and compressed files. Remove obsolete compressed files to optimize project structure.

--- obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4935e1223bdebd51ee6a4c362e5875f87fd88ddc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5c8e031c03820a508e72723e935a703f3c1243d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
